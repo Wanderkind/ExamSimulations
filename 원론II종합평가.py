6626f325cb3622e86a94cda624da6fc27757a0b7
Jeongseop Lee <wanderkind@semyung.ac.kr>
2021-12-15 17:01:49 +0900

    Update 원론II종합평가.py

--- 원론II종합평가.py
+++ 원론II종합평가.py
@@ -78,10 +78,14 @@ while True:
                 if 답안 == '':
                     continue
                 
+                elif 답안 == 'X' or 답안 == 'x' or 답안 == 'ㅌ':
+                    cease = 'yes'
+                    break
+                
                 elif len(답안) == 1:
                     if 49 <= ord(str(답안)) < 54:
                         if 선지[int(답안) - 1] == 정답:
-                            print('\n정답\n')
+                            print('\n정답\n\n뒤로 돌아가려면 X를 입력하세요')
                             break
                         
                         else:
@@ -91,10 +95,6 @@ while True:
                     else:
                         print('\n오답')
                         continue
-                
-                elif 답안 == 'X' or 답안 == 'x' or 답안 == 'ㅌ':
-                    cease = 'yes'
-                    break
                 
                 else:
                     print('\n오답')
@@ -184,10 +184,14 @@ while True:
                 if 답안 == '':
                     continue
                 
+                elif 답안 == 'X' or 답안 == 'x' or 답안 == 'ㅌ':
+                    cease = 'yes'
+                    break
+                
                 elif len(답안) == 1:
                     if 49 <= ord(str(답안)) < 54:
                         if 선지[int(답안) - 1] == 정답:
-                            print('\n정답\n')
+                            print('\n정답\n\n뒤로 돌아가려면 X를 입력하세요')
                             break
                         
                         else:
@@ -197,10 +201,6 @@ while True:
                     else:
                         print('\n오답')
                         continue
-                
-                elif 답안 == 'X' or 답안 == 'x' or 답안 == 'ㅌ':
-                    cease = 'yes'
-                    break
                 
                 else:
                     print('\n오답')
