38915ffa2d76d0a85dfb78238fb1ad1df6a10805
Jeongseop Lee <wanderkind@semyung.ac.kr>
2022-05-23 00:02:31 +0900

    Create 오운의평기태과불급.py

---
+++ 오운의평기태과불급.py
@@ -0,0 +1,20 @@
+import random
+
+S=[
+['敷和','發生','委和'],
+['升明','赫曦','伏明'],
+['備化','敦阜','卑監'],
+['審平','堅成','從革'],
+['靜順','流衍','涸流']]
+
+while 1:
+    a=random.randrange(5)
+    b=random.randrange(3)
+    print(f"{['木','火','土','金','水'][a]}의 {['平氣','太過','不及'][b]}")
+    while 1:
+        if input()==S[a][b]:
+            print('정답')
+            break
+        else:
+            print('ㄴㄴ')
+            continue
