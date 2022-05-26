f436f8d0d4284b90669caf13f8442015080cd2ba
Jeongseop Lee <wanderkind@semyung.ac.kr>
2022-05-26 09:49:52 +0900

    Create 대표적지방산.py

---
+++ 대표적지방산.py
@@ -0,0 +1,11 @@
+a=['lauric','myristic','palmitic','stearic','arachidic','palmitoleic','oleic','linoleic','linolenic','arachidonic']
+while 1:
+    s=[]
+    for i in range(10):
+        s.append(input(str(i+1)+' '))
+    if s==a:
+        print('ㅇㅇㅇㅇㅇ')
+        break
+    else:
+        for _ in range(50):print('ㄴㄴㄴ\n')
+        continue
