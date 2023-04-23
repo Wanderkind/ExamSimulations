3c1ea7d6c3363b42836e3b5c8b1a316d4dc35a0b
Jeongseop Lee <wanderkind@semyung.ac.kr>
2023-04-24 07:06:57 +0900

    Update 본초_format.py

--- 본초_format.py
+++ 본초_format.py
@@ -1,29 +1,36 @@
 from random import *
 
-queue = []
-count = 0
+z = [*range(116)]
+shuffle(z)
 
-while True:
+while z:
     
-    count += 1
-    q, c, a = choice(l)
-    x = len(c)
-    print("Q.", q)
-    s = c[:]
-    shuffle(s)
-    for i in range(x):
-        print(f"{i + 1}. {s[i]}")
-    print()
-    i = s.index(a) + 1
-    firsttry = True
+    for h in z:
+        
+        q, c, a = l[h]
+        x = len(c)
+        print("Q.", q)
+        s = c[:]
+        shuffle(s)
+        for i in range(x):
+            print(f"{i + 1}. {s[i]}")
+        print()
+        i = s.index(a) + 1
+        firsttry = True
+        
+        while True:
+            try:
+                response = int(input(f"답 (1 ~ {x}) > "))
+                if response == i:
+                    print("\n정답")
+                    break
+                firsttry = False
+                print("오답\n")
+            except ValueError:
+                True
+        
+        if firsttry:
+            z.remove(h)
+        print(f"맞힌 개수: {116 - len(z)}/116\n")
     
-    while True:
-        response = int(input(f"답 (1 ~ {x}) > "))
-        if response == i:
-            print("\n정답")
-            break
-        firsttry = False
-        print("오답\n")
-    
-    queue.append(1 if firsttry else 0)
-    print(f"지난 20개 정답률: {sum(queue[-20:])}/{min(count, 20)}\n")
+    print(f"아직 {len(z)}개 틀렸음\n이제 틀린 거 다시 풀어보자\n")
